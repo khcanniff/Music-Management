73d477e764f79fb0fb9f21bdb9d567a5b50f64fc
khcanniff <k_canniff@u.pacific.edu>
2019-12-18 23:34:41 -0800

    hi

---
+++ Assets/Scenes/scripts/AudioClass.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClass : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] tracks;
+
+    // the below AudioSource is used for managing their own tracks. so for example, M1 should handle playing of the M1 tracks etc
+    private AudioSource audio;
+
+
+    int Counter = 0;
+ 
+
+    // Start is called before the first frame update
+    public void Setup()
+    {
+        audio = gameObject.AddComponent<AudioSource>();
+        audio.clip = tracks[Counter];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool checkIfAudioIsPlaying()
+    {
+        return audio.isPlaying;
+    }
+
+
+    public void playNextSound()
+    {
+        if (!checkIfAudioIsPlaying() && Counter < tracks.Length)
+        {
+            audio.clip = tracks[Counter];
+            Counter++;
+        }
+    }
+
+}
---
+++ Assets/Scenes/scripts/AudioManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioClass))]
+public class AudioManagement : MonoBehaviour
+{
+    private AudioClass M1;
+    private AudioClass M2;
+    private AudioClass M3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        M1 = this.GetComponent<AudioClass>();
+        M1.Setup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void M1Play()
+    {
+        M1.playNextSound();
+    }
+}
